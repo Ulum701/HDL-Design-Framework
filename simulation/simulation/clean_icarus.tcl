599582925a76ec5f688dad5daa17a88bbb07672e
Ulum701 <93436530+Ulum701@users.noreply.github.com>
2026-09-23 09:04:06 +0700

    Delete simulation/simulation directory

--- simulation/simulation/clean_icarus.tcl
+++
@@ -1,3 +0,0 @@
-    #=== Delete all .vcd and .vvp files 
-    file delete {*}[glob -nocomplain *.vcd *.vvp]
-    puts "\[SUCCESS\] Successfully clean icarus"
--- simulation/simulation/clean_questa.tcl
+++
@@ -1,7 +0,0 @@
-
-    # -force is required to delete non-empty directories
-    file delete -force work
-    file delete transcript
-    file delete vsim.wlf
-    file delete modelsim.ini
-    puts "\[SUCCESS\] Successfully clean questa"
